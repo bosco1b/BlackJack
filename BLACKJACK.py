7b6ab5951d5babc78abc2698ecc1d711ed0e0f71
bosco1b <boscao10@gmail.com>
2020-05-25 23:12:09 -0300

    Update BLACKJACK.py

--- BLACKJACK.py
+++ BLACKJACK.py
@@ -21,8 +21,7 @@ def primeiraDistribuição():
         dealer.append(baralho.pop())
 
 
-
-def resultado():
+def resultadoJogador():
     Y = 0
     while Y < len(jogador):
         x = jogador[Y]
@@ -42,47 +41,84 @@ def resultado():
             jogador.pop(pos)
             jogador.insert(pos, 10)
 
-        if ("A") in jogador:
+        elif ("A") in jogador:
             pos = jogador.index("A")
             jogador.pop(pos)
-            global onze
+
             onze = int(input("Escolha o valor do AS (1 ou 11)"))
             if onze == 1:
                 jogador.insert(pos, 1)
+            elif onze == 11:
+                jogador.insert(pos, 11)
+
+
+def resultadoDealer():
+    Y = 0
+    while Y < len(dealer):
+        x = dealer[Y]
+        Y += 1
+
+    for x in dealer:
+        if ("J") in dealer:
+            pos = dealer.index("J")
+            dealer.pop(pos)
+            dealer.insert(pos, 10)
+        elif ("Q") in dealer:
+            pos = dealer.index("Q")
+            dealer.pop(pos)
+            dealer.insert(pos, 10)
+        elif ("K") in dealer:
+            pos = dealer.index("K")
+            dealer.pop(pos)
+            dealer.insert(pos, 10)
+
+        if ("A") in dealer:
+            pos = dealer.index("A")
+            dealer.pop(pos)
+            global onze
+
+            if sum(dealer) < 11:
+                jogador.insert(pos, 1)
             else:
                 jogador.insert(pos, 11)
+
 
 def nova_compra():
     while sum(jogador) < 21:
 
         compra = (input('Deseja comprar mais uma carta?(SIM/NÃO)'))
-        if compra.upper() == 'SIM' :
+        if compra.upper() == 'SIM':
 
             jogador.append(baralho.pop())
             print("A carta foi {}".format(jogador[-1]))
-            resultado()
+            resultadoJogador()
         else:
             print("Você parou com {} pontos!".format(sum(jogador)))
             break
 
 
-
-
-
-      
-
-
-
+def IADealer():
+    while sum(dealer) < 16:
+        dealer.append(baralho.pop())
+        resultadoDealer()
 
 
 inicio()
-resultado()
+resultadoJogador()
 nova_compra()
+resultadoDealer()
+IADealer()
 print(jogador)
-
+print(dealer)
 
 if sum(jogador) > 21:
     print("OPS!SUA SOMA DEU {} E VOCÊ ESTOUROU!".format(sum(jogador)))
-
-
+elif 21 > sum(jogador) > sum(dealer):
+    print("Você venceu com {} pontos!".format(sum(jogador)))
+elif sum(jogador) == sum(dealer):
+    print("Empate! Você empatou com o dealer, ambos com {}".format(sum(dealer)))
+elif 21 > sum(dealer) > sum(jogador):
+    print("Vitoria do dealer com {} pontos! Você fez apenas {}".format(sum(dealer), sum(jogador)))
+elif sum(dealer) > 21:
+    print("O dealer estourou e você venceu!")
 
